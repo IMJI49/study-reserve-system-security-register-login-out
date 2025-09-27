e09849ef920e2ceb55b599b9f5586eb4d2b50af3
지성현 <j_sh01@naver.com>
2025-09-27 20:22:32 +0900

    [feat] BaseEntity,css

--- src/main/java/com/choongang/studyreservesystem/domain/User.java
+++ src/main/java/com/choongang/studyreservesystem/domain/User.java
@@ -1,21 +1,22 @@
 package com.choongang.studyreservesystem.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Getter
-public class User {
+public class User extends BaseEntity{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
